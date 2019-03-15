fcd1f20098814c3d75780b426e4ba4c88e59f60d
mayoctt@gmail.com <46900928+asilingas@users.noreply.github.com>
2019-03-15 22:46:32 +0200

    first commit

---
+++ play.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__.'/lib/Ship.php';
+
+function printShipSummary($someShip)
+{
+    echo 'Ship name: '.$someShip->name;
+    echo "<hr>";
+    $someShip->sayHello();
+    echo "<hr>";
+    echo $someShip->getName();
+    echo "<hr>";
+    echo $someShip->getNameAndSpecs(false);
+    echo "<hr>";
+    echo $someShip->getNameAndSpecs(true);
+}
+
+$myShip = new Ship();
+$myShip->name = "Jedi Starpship";
+$myShip->weaponPower = 10;
+$myShip->strength = 100;
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+printShipSummary($myShip);
+echo "<hr>";
+printShipSummary($otherShip);
+echo "<hr>";
+
+if ($myShip->doesGivenShipHasMoreStrength($otherShip)) {
+    echo $otherShip->name.' has more strength';
+} else {
+    echo $myShip->name.' has more strength';
+}
